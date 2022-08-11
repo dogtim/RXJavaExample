804b730f7cd816cf1f7d6d45973296a53ed00be3
HsiaoTing Chen <fatfattim@gmail.com>
2022-08-11 18:17:22 +0800

    Add Observable.just for fun

--- app/src/test/java/com/example/rxjava/ExampleUnitTest.kt
+++ app/src/test/java/com/example/rxjava/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.example.rxjava
 
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +15,28 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun just() {
+        val observables = Observable.just("123", "456")
+        val observer: Observer<String> = object : Observer<String> {
+            override fun onSubscribe(d: Disposable) {
+                println(d)
+            }
+
+            override fun onNext(s: String) {
+                println(s)
+            }
+
+            override fun onError(e: Throwable) {
+                println(e.localizedMessage)
+            }
+
+            override fun onComplete() {
+                println("complete")
+            }
+        }
+        observables.subscribe(observer)
     }
 }
