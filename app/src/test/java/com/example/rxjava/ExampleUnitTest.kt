15cf2ccda4246fa24d60a57ed828633b78f664d3
HsiaoTing Chen <fatfattim@gmail.com>
2022-08-16 14:50:32 +0800

    Practice the map in RxJava

--- app/src/test/java/com/example/rxjava/ExampleUnitTest.kt
+++ app/src/test/java/com/example/rxjava/ExampleUnitTest.kt
@@ -1,10 +1,13 @@
 package com.example.rxjava
 
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.ObservableEmitter
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.functions.Cancellable
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.regex.Pattern
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -38,5 +41,42 @@ class ExampleUnitTest {
             }
         }
         observables.subscribe(observer)
+    }
+
+    @Test
+    fun map() {
+        val expirationDatePattern: Pattern = Pattern.compile("^\\d\\d/\\d\\d$")
+        val creditCardExpirationDate = Observable.create<String> {  emitter ->
+            emitter.onNext("Kotlin")
+            emitter.onNext("Java")
+            emitter.onNext("Python")
+            emitter.onNext("Javascript")
+            emitter.onNext("Go")
+            emitter.onNext("C")
+            emitter.onNext("Rust")
+            emitter.onComplete()
+
+        }
+        val isValidExpirationDate: Observable<Boolean> = creditCardExpirationDate
+            .map { text -> expirationDatePattern.matcher(text).find() }
+
+        val observer: Observer<Boolean> = object : Observer<Boolean> {
+            override fun onSubscribe(d: Disposable) {
+                println(d)
+            }
+
+            override fun onNext(s: Boolean) {
+                println(s)
+            }
+
+            override fun onError(e: Throwable) {
+                println(e.localizedMessage)
+            }
+
+            override fun onComplete() {
+                println("complete")
+            }
+        }
+        isValidExpirationDate.subscribe(observer)
     }
 }
