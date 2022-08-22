f114c65aca6dfcd01786b71e5648bfe6ef253afc
HsiaoTing Chen <fatfattim@gmail.com>
2022-08-22 17:11:41 +0800

    Add flatMap test case

--- app/src/test/java/com/example/rxjava/ExampleUnitTest.kt
+++ app/src/test/java/com/example/rxjava/ExampleUnitTest.kt
@@ -1,13 +1,18 @@
 package com.example.rxjava
 
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.ObservableEmitter
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
-import io.reactivex.rxjava3.functions.Cancellable
+import io.reactivex.rxjava3.schedulers.TestScheduler
+import org.hamcrest.CoreMatchers.hasItems
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
+import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
+import kotlin.collections.ArrayList
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -55,8 +60,8 @@ class ExampleUnitTest {
             emitter.onNext("C")
             emitter.onNext("Rust")
             emitter.onComplete()
-
         }
+
         val isValidExpirationDate: Observable<Boolean> = creditCardExpirationDate
             .map { text -> expirationDatePattern.matcher(text).find() }
 
@@ -77,6 +82,45 @@ class ExampleUnitTest {
                 println("complete")
             }
         }
+
         isValidExpirationDate.subscribe(observer)
+    }
+
+    @Test
+    fun flatMap() {
+        // The flatMap operator converts each item returned from a source observable into an independent observable
+        // using the supplied function and then merges all the observables into a single observable.
+
+        val actualOutput: MutableList<String> = ArrayList()
+        val scheduler = TestScheduler()
+        val keywordToSearch: List<String> = listOf("b", "bo", "boo", "book", "books")
+        var delayHook = true
+        Observable.fromIterable(keywordToSearch)
+            .flatMap { s: String ->
+                // This is key operation which help me to understand the delay function and simulate the first in but later out for asynchronous task
+                val delayTime: Long = if (delayHook) 10 else 5
+                delayHook = !delayHook
+                Observable.just(
+                    "$s MockItem1",
+                    "$s MockItem2"
+                ).delay(delayTime, TimeUnit.SECONDS, scheduler)
+            }
+            .toList()
+            .doOnSuccess {
+                actualOutput.addAll(it)
+            }
+            .subscribe()
+
+        scheduler.advanceTimeBy(1, TimeUnit.MINUTES)
+
+        assertThat(
+            actualOutput, hasItems(
+                "bo MockItem1", "bo MockItem2",
+                "book MockItem1", "book MockItem2",
+                "b MockItem1", "b MockItem2",
+                "boo MockItem1", "boo MockItem2",
+                "books MockItem1", "books MockItem2"
+            )
+        )
     }
 }
