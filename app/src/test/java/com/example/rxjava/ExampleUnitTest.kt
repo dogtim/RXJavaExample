031abacf6c5a9b6ef70261eb25f135b4ad34e764
HsiaoTing Chen <fatfattim@gmail.com>
2022-08-23 10:37:20 +0800

    Add SwitchMap test

--- app/src/test/java/com/example/rxjava/ExampleUnitTest.kt
+++ app/src/test/java/com/example/rxjava/ExampleUnitTest.kt
@@ -3,6 +3,8 @@ package com.example.rxjava
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.internal.operators.observable.ObservableFromIterable
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
 import io.reactivex.rxjava3.schedulers.TestScheduler
 import org.hamcrest.CoreMatchers.hasItems
 import org.hamcrest.MatcherAssert.assertThat
@@ -11,7 +13,6 @@ import org.junit.Test
 import java.util.*
 import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
-import kotlin.collections.ArrayList
 
 
 /**
@@ -51,7 +52,7 @@ class ExampleUnitTest {
     @Test
     fun map() {
         val expirationDatePattern: Pattern = Pattern.compile("^\\d\\d/\\d\\d$")
-        val creditCardExpirationDate = Observable.create<String> {  emitter ->
+        val creditCardExpirationDate = Observable.create<String> { emitter ->
             emitter.onNext("Kotlin")
             emitter.onNext("Java")
             emitter.onNext("Python")
@@ -86,39 +87,90 @@ class ExampleUnitTest {
         isValidExpirationDate.subscribe(observer)
     }
 
-    @Test
-    fun flatMap() {
-        // The flatMap operator converts each item returned from a source observable into an independent observable
-        // using the supplied function and then merges all the observables into a single observable.
+    private fun flatmap1() {
 
-        val actualOutput: MutableList<String> = ArrayList()
-        val scheduler = TestScheduler()
-        val keywordToSearch: List<String> = listOf("b", "bo", "boo", "book", "books")
+    }
+
+    fun makeFlatMap(source: Observable<String>, scheduler: TestScheduler): Observable<String> {
         var delayHook = true
-        Observable.fromIterable(keywordToSearch)
-            .flatMap { s: String ->
-                // This is key operation which help me to understand the delay function and simulate the first in but later out for asynchronous task
-                val delayTime: Long = if (delayHook) 10 else 5
-                delayHook = !delayHook
-                Observable.just(
-                    "$s MockItem1",
-                    "$s MockItem2"
-                ).delay(delayTime, TimeUnit.SECONDS, scheduler)
-            }
+        val result = source.flatMap { s ->
+            val delayTime: Long = if (delayHook) 10 else 5
+            delayHook = !delayHook
+            Observable.just(
+                "$s MockItem1",
+                "$s MockItem2"
+            ).delay(delayTime, TimeUnit.SECONDS, scheduler)
+        }
+
+        return result
+    }
+
+    fun makeSwitchMap(source: Observable<String>, scheduler: TestScheduler): Observable<String> {
+        var delayHook = true
+        val result = source.switchMap { s ->
+            val delayTime: Long = if (delayHook) 10 else 5
+            delayHook = !delayHook
+            Observable.just(
+                "$s MockItem1",
+                "$s MockItem2"
+            ).delay(delayTime, TimeUnit.SECONDS, scheduler)
+        }
+
+        return result
+    }
+
+    fun testMap(isFlatMap: Boolean): MutableList<String> {
+        val scheduler = TestScheduler()
+        val actualOutput: MutableList<String> = ArrayList()
+        val keywordToSearch: List<String> = listOf("b", "bo", "boo", "book", "books")
+        val observable = if (isFlatMap) {
+            makeFlatMap(Observable.fromIterable(keywordToSearch), scheduler)
+        } else {
+            makeSwitchMap(Observable.fromIterable(keywordToSearch), scheduler)
+        }
+        observable
             .toList()
             .doOnSuccess {
                 actualOutput.addAll(it)
             }
             .subscribe()
-
         scheduler.advanceTimeBy(1, TimeUnit.MINUTES)
 
+        return actualOutput
+
+    }
+
+
+    /**
+     * The flatMap operator converts each item returned from a source observable into an independent observable
+     *
+     * using the supplied function and then merges all the observables into a single observable.
+     *
+     */
+    @Test
+    fun flatMap() {
         assertThat(
-            actualOutput, hasItems(
+            testMap(true), hasItems(
                 "bo MockItem1", "bo MockItem2",
                 "book MockItem1", "book MockItem2",
                 "b MockItem1", "b MockItem2",
                 "boo MockItem1", "boo MockItem2",
+                "books MockItem1", "books MockItem2"
+            )
+        )
+    }
+
+
+    /**
+     * The switchMap operator is similar to flatMap, except that it retains the result of only the latest observable, discarding the previous ones.
+     *
+     * using the supplied function and then merges all the observables into a single observable.
+     *
+     */
+    @Test
+    fun switchMap() {
+        assertThat(
+            testMap(false), hasItems(
                 "books MockItem1", "books MockItem2"
             )
         )
